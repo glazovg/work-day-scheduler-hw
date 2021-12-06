86530029797128599244a8b4d75d66cc6d065bbb
Guillermo Lazo <glazovg@gmail.com>
2021-12-06 12:08:38 -0600

    Adding responsiveness to time blocks and background color by script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
 let currentDay = moment().format('dddd, MMMM Do');
+let currentHour = moment().format('HH');
 let currentDayEl = $('#currentDay');
+let timeBlockEls = $('.time-block div');
 
 currentDayEl.text(currentDay);
+
+timeBlockEls.each(function (index) {
+    let timeBlockTimeEl = moment($(this).text(), ['hA']).format('HH');
+    let textAreaEl = $(this).parent().find('textarea');
+
+    if (timeBlockTimeEl < currentHour) {
+        textAreaEl.addClass('past');
+        textAreaEl.prop('readonly', true);
+    } else if (timeBlockTimeEl === currentHour) {
+        textAreaEl.addClass('present');
+    } else {
+        textAreaEl.addClass('future');
+    }
+})
+
